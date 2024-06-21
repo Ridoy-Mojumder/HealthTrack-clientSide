93815463b9ff4805faf7344673dca134ff554c1d
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-06-21 20:44:43 +0600

    modify all test

--- src/Dashboard/Home/AllTestForHome.jsx
+++ src/Dashboard/Home/AllTestForHome.jsx
@@ -73,16 +73,7 @@ const AllTestForHome = () => {
                             </div>
                         ))}
                     </div>
-                    {visibleTests < tests.length && (
-                        <div className="flex justify-center mt-6">
-                            <button
-                                onClick={showMoreTests}
-                                className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                            >
-                                Show More
-                            </button>
-                        </div>
-                    )}
+                    
                 </div>
             )}
         </div>
