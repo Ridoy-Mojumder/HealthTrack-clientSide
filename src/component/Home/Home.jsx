3825841efe49f315bf5c0f45582affa511a1d3b4
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-06-22 02:40:49 +0600

    modify home

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -12,7 +12,6 @@ const Home = () => {
                 <title>Home - HealthTrack Diagnostic Center</title>
                 <meta name="description" content="Contact us to get in touch with our diagnostic center. Leave your comments and feedback." />
             </Helmet>
-            <HomeForBanner></HomeForBanner>
 
             <AllTestForHome></AllTestForHome>
             <div>
