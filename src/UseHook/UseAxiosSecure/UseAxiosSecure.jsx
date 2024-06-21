b1f3e53ed8db3d4f98aca0f658c3c9b2216271a8
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-06-22 02:30:27 +0600

    also modify it

--- src/UseHook/UseAxiosSecure/UseAxiosSecure.jsx
+++ src/UseHook/UseAxiosSecure/UseAxiosSecure.jsx
@@ -12,7 +12,16 @@ const UseAxiosSecure = () => {
     const navigate = useNavigate();
     const { logOut } = useContext(AuthContext);
 
-   
+    // request interceptor to add authorization header for every secure call to teh api
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        // console.log('request stopped by interceptors', token)
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
 
 
     // intercepts 401 and 403 status
